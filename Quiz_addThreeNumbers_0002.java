212661938d8293840b7e873e47e7e631440cb0ac
Makthala Sai Teja Goud <118780958+Saiteja1102@users.noreply.github.com>
2024-01-15 19:57:19 +0530

    01

---
+++ Quiz_addThreeNumbers_0002.java
@@ -0,0 +1,10 @@
+public class Quiz_addThreeNumbers_0002 {
+    public static void main(String[] args){
+        int num1 = 6;
+        int num2 = 12;
+        int num3 = 23;
+        System.out.print("The sum of three numbers is: ");
+        int sum = num1+num2+num3;
+        System.out.print(sum);
+    }
+}
---
+++ hello_0001.java
@@ -0,0 +1,16 @@
+public class hello_0001 {
+    public static void main(String[]args) {
+        System.out.print("Hello World");
+    }
+}
+
+
+// --> single line comments
+
+// Naming Convention
+
+// add two numbers
+// ---> For classes we use PascalConvention
+// AddTwoNumbers
+// ---> For Functions we use camelCaseConvention
+// addTwoNumbers
