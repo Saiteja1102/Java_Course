f670a12d7678d40750cc2733b939136dc488949b
Makthala Sai Teja Goud <118780958+Saiteja1102@users.noreply.github.com>
2024-01-16 18:42:10 +0530

    02

---
+++ Java_0004_TakingInput.java
@@ -0,0 +1,68 @@
+
+// Taking Input Methods
+
+// 1) Import Scanner  --> import.java.util.Scanner
+// 2) Make a new Scanner --> Scanner (NameOfScanner) = new Scanner(System.in)
+
+import java.util.Scanner;
+
+public class Java_0004_TakingInput {
+    public static void main(String[] arg) {
+        System.out.println("Taking input From the user...");
+
+        // Making a new Scanner
+        Scanner sai = new Scanner(System.in);
+
+        // Taking input of integer numbers
+        System.out.print("Enter integer number: ");
+        int a = sai.nextInt();
+        System.out.println(a);
+        
+        // Taking input of float poing numbers
+        System.out.print("Enter float number: ");
+        float f = sai.nextFloat();
+        System.out.println(f);
+
+        // Taking input of String
+        System.out.print("Enter your name: ");
+        String s = sai.next();
+        System.out.println(s); // Spaces are not taken
+
+        // Taking input of Short numbers
+        System.out.print("Enter short number: ");
+        short sh = sai.nextShort();
+        System.out.println(sh);
+
+        // Taking input of byte numbers
+        System.out.print("Enter Byte number: ");
+        byte b = sai.nextByte();
+        System.out.println(b);
+
+        // Taking input of long number
+        System.out.print("Enter long Number: ");
+        long l = sai.nextLong();
+        System.out.println(l);
+
+        // Taking input of double number
+        System.out.print("Enter double Number: ");
+        double d = sai.nextDouble();
+        System.out.println(d);
+
+        // Taking input of character
+        /* 
+        To read a char, we use next().charAt(0). next() function returns the next token/word in the input as a string and charAt(0) 
+        function returns the first character in that string, the number 0 in the function in CharAt(NUMBER)  represents the index 
+        of the single word of the string taken input, and set that index character to the char variable.
+        */
+        System.out.print("Enter character: ");
+        // Format
+        // (ScannerName).next().charAt(//Inder number of the character: )
+        char c = sai.next().charAt(0); // 
+        System.out.println(c);
+
+        // Taking input of boolean
+        System.out.print("Enter boolean: ");
+        boolean bo = sai.nextBoolean();
+        System.out.println(bo);
+    }
+}
---
+++ Java_0005_BoardExamPercentageClaculator_Ex1.java
@@ -0,0 +1,35 @@
+
+/*
+Excerise 1: Write a program to calculate percentage of a given student in CBSE board exam. 
+His marks from 5 subjects must be taken as input from the keyboard ?
+*/
+
+
+import java.util.Scanner;
+
+public class Java_0005_BoardExamPercentageClaculator_Ex1 {
+    public static void main(String[] arg) {
+        System.out.println("Board Exam Percentage calculator...");
+        System.out.println("");
+        System.out.println("");
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your total marks per subject: ");
+        int totalMarks = input.nextInt();
+
+        System.out.print("Enter Subject 1 marks: ");
+        float sub1 = input.nextFloat();
+        System.out.print("Enter Subject 2 marks: ");
+        float sub2 = input.nextFloat();
+        System.out.print("Enter Suject 3 marks: ");
+        float sub3 = input.nextFloat();
+        System.out.print("Enter Suject 4 marks: ");
+        float sub4 = input.nextFloat();
+        System.out.print("Enter Suject 5 marks: ");
+        float sub5 = input.nextFloat();
+
+        float percentage = ((sub1 + sub2 + sub3 + sub4 + sub5)/(totalMarks*5)*100);
+        System.out.println("Percentage: "+percentage+" %");
+    }
+}
