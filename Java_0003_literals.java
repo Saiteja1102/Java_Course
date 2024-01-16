1e2ad72d6d7750debacfa26dff9fd012ed036896
Makthala Sai Teja Goud <118780958+Saiteja1102@users.noreply.github.com>
2024-01-16 17:18:29 +0530

    02

---
+++ Java_0003_literals.java
@@ -0,0 +1,30 @@
+
+// Literals in Java
+// Constant value is called literals
+// Example : int age = 19; // here 19 is called litteral
+
+
+public class Java_0003_literals {
+    public static void main(String[] args) {
+        int a = 11;
+        byte b = 11;
+        short c = 1111;
+        long d = 123212343444L; // if we not write 'l' default it will be integer so after writing 'l' it becomes double 
+        char e = 'S'; // For character you have to use single inverted commas ' '
+        float f = 3.14f; // if we not write 'f' default it will be double so after writing 'f' it becomes float
+        double g = 3.1234; // Default it will be double, no need to write 'd' but if we want we can write
+        boolean h = true; // Either true or false
+        String i = "Saiteja"; // For string u have to use double inverted commas " "
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
